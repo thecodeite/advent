0c86280b66f7ba35539f440caf0b77a43500ee36
Sam Plews <the@codeite.com>
2015-12-07 12:42:48 +0000

    Day 6 - Part 2

--- day6/day6.js
+++ day6/day6.js
@@ -2,9 +2,11 @@ const readLineData = require('../dataReader').readLineData;
 const data = readLineData();
 //console.log(data);
 
-const size = 1000;
+const zoom = 1;
+
+const size = 1000/zoom;
 const grid = new Array(size*size);
-for(var i=0; i<size*size; i++) grid[i] = false;
+for(var i=0; i<size*size; i++) grid[i] = 0;
 
 const instructions = data
   .map(parseRow);
@@ -20,7 +22,7 @@ instructions.forEach(applyInst);
 
 render(grid);
 
-console.log(grid.filter(x => x).length);
+console.log(grid.reduce((p,c) => p+c, 0));
 
 function applyInst(inst) {
   //console.log(inst.from.x, inst.from.y, '->', inst.to.x, inst.to.y);
@@ -33,22 +35,31 @@ function applyInst(inst) {
 }
 
 function render(grid) {
+  console.log(`<style>
+    p, b {border: 0; margin: 0; display: block; width: 1px; height: 1px; }
+    `);
+    for(var i=0; i<256; i+=4) {
+      console.log(`.b${i/4} {background-color: rgb(${i},${256-i},0)}`);
+    }
+    console.log(`div { display: flex; }
+  </style><p>`)
   for(var y = 0; y <= size; y++) {
     var row = grid.slice(y*size, (y+1)*size);
-    console.log(row.map(b=>b?'o':'.').join(''));
+    console.log('<div>'+row.map(b=>'<p class="b'+b+'"></p>').join('')+'</div>\n');
   }
+  console.log('</p>');
 }
 
 function parseRow(row) {
   const action = {};
   if(row.startsWith('turn off ')) {
-    action.do = _ => false;
+    action.do = x => x === 0?0:x-1;
     row = row.substr('turn off '.length);
   } else if(row.startsWith('turn on ')) {
-    action.do = _ => true;
+    action.do = x => x+1;
     row = row.substr('turn on '.length);
   } else if(row.startsWith('toggle ')) {
-    action.do = x => !x;
+    action.do = x => x+2;
     row = row.substr('toggle '.length);
   }
 
@@ -61,7 +72,7 @@ function parseRow(row) {
 
 function parseCord(cord) {
   return {
-    x: ~~(cord.split(',')[0]),
-    y: ~~(cord.split(',')[1])
+    x: ~~(cord.split(',')[0]/zoom),
+    y: ~~(cord.split(',')[1]/zoom)
   };
 }
